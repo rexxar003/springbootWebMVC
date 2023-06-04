03adcd0e8f3fe19a8f11dd97e9beb82c3222dc57
Kanza Az Zahrawani <09021382227157@studen.unsri.ac.id>
2023-06-04 09:47:27 +0700

    reques dan response web

--- src/main/java/programmerzamannow/webMVC/controller/HelloController.java
+++ src/main/java/programmerzamannow/webMVC/controller/HelloController.java
@@ -1,17 +1,24 @@
 package programmerzamannow.webMVC.controller;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class HelloController {
 
     @RequestMapping(path = "/hello")
-    public void helloWorld(HttpServletResponse response) throws IOException {
-        response.getWriter().println("hello world");
+    public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("name");
+
+        if (Objects.isNull(name)) {
+            name = "guest";
+        }
+        response.getWriter().println("hello world " + name);
     }
 }
