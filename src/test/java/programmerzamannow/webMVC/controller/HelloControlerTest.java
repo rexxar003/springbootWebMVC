a84bbd5c1d4ec4d354323cc21a7bfc8d0705d4a7
Kanza Az Zahrawani <09021382227157@studen.unsri.ac.id>
2023-06-04 10:13:30 +0700

    melakukan pengetesan request dan response

---
+++ src/test/java/programmerzamannow/webMVC/controller/HelloControlerTest.java
@@ -0,0 +1,40 @@
+package programmerzamannow.webMVC.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.matches;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControlerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void helloGuestTest() throws Exception {
+        mockMvc.perform(
+                get("/hello")).andExpectAll(status().isOk(),
+                        content().string(Matchers.containsString("hello world guest")));
+    }
+
+    @Test
+    public void hellonameTest() throws Exception {
+        mockMvc.perform(
+                get("/hello").queryParam("name", "kanza"))
+                .andExpectAll(status().isOk(), content().string(Matchers.containsString("hello world kanza")));
+    }
+
+}
