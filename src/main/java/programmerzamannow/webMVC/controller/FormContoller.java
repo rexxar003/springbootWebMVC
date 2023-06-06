fb73b5a5624bf99005b58674d7451d6d212aa38b
Kanza Az Zahrawani <09021382227157@studen.unsri.ac.id>
2023-06-06 17:47:46 +0700

    menggunakan @requestparam untuk form request

--- src/main/java/programmerzamannow/webMVC/controller/FormContoller.java
+++ src/main/java/programmerzamannow/webMVC/controller/FormContoller.java
@@ -1,7 +1,11 @@
 package programmerzamannow.webMVC.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,5 +23,17 @@ public class FormContoller {
                 </body>
                 </html>
                     """.replace("$name", name);
+    }
+
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    @PostMapping(path = "/form/person", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseBody
+    public String createPerson(
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "birthDate") Date birthDate,
+            @RequestParam(name = "address") String address) {
+        return "succes create person with name :" + name + "birthDate : " + simpleDateFormat.format(birthDate) +
+                "address : " + address;
     }
 }
--- src/test/java/programmerzamannow/webMVC/controller/FormControllerTest.java
+++ src/test/java/programmerzamannow/webMVC/controller/FormControllerTest.java
@@ -26,13 +26,25 @@ public class FormControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-   @Test
-   void formHello() throws Exception{
-    mockMvc.perform(post("/form/hello")
-    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-    .param("name", "kanza")
-    ).andExpectAll(status().isOk(),
-    header().string(org.springframework.http.HttpHeaders.CONTENT_TYPE, Matchers.containsString(MediaType.TEXT_HTML_VALUE))
-    ,content().string(Matchers.containsString("Hello kanza")));
-   }
+    @Test
+    void formHello() throws Exception {
+        mockMvc.perform(post("/form/hello")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("name", "kanza")).andExpectAll(status().isOk(),
+                        header().string(org.springframework.http.HttpHeaders.CONTENT_TYPE,
+                                Matchers.containsString(MediaType.TEXT_HTML_VALUE)),
+                        content().string(Matchers.containsString("Hello kanza")));
+    }
+
+    @Test
+    void createPerson() throws Exception {
+        mockMvc.perform(post("/form/person").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("name", "kanza")
+                .param("birthDate", "2003-09-28")
+                .param("address", "Indonesia"))
+                .andExpectAll(status().isOk(), content().string(
+                        Matchers.containsString(
+                                "succes create person with name :kanzabirthDate : 2003-09-28address : Indonesia")));
+    }
+
 }
