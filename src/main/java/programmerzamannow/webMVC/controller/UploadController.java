9fac36c0efd066888e306594557b48a5213bbd90
Kanza Az Zahrawani <09021382227157@studen.unsri.ac.id>
2023-06-06 18:25:35 +0700

    upload file

---
+++ src/main/java/programmerzamannow/webMVC/controller/UploadController.java
@@ -0,0 +1,30 @@
+package programmerzamannow.webMVC.controller;
+
+import java.io.IOException;
+
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.jayway.jsonpath.internal.Path;
+
+@Controller
+public class UploadController {
+
+    @PostMapping(path = "/upload/profile", consumes = org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public String upload(
+            @RequestParam(name = "name") String name,
+            @RequestPart(name = "profile" ) MultipartFile file) throws IllegalStateException, IOException {
+        java.nio.file.Path path = java.nio.file.Path.of("upload/" + file.getOriginalFilename());
+
+        file.transferTo(path);
+
+        return "succes save profile "+name + " to " + path;
+    }
+
+}
---
+++ src/test/java/programmerzamannow/webMVC/controller/UploadControllerTest.java
@@ -0,0 +1,43 @@
+package programmerzamannow.webMVC.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.matches;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.io.IOException;
+
+import javax.print.attribute.standard.Media;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.matches;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UploadControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test 
+    void uploadTest() throws IOException, Exception{
+        mockMvc.perform(multipart("/upload/profile")
+        .file(new MockMultipartFile("profile", "profile.png","image/png",
+         getClass().getResourceAsStream("/images/profile.png"))).param("name", "kanza")
+         .contentType(MediaType.MULTIPART_FORM_DATA)).andExpectAll(status().isOk(), 
+         content().string(Matchers.containsString("succes save profile kanza to upload\\profile.png")));
+    }
+}
