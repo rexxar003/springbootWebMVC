aef70017b04073473a4d74688895f03aed298bd9
Kanza Az Zahrawani <09021382227157@studen.unsri.ac.id>
2023-06-09 09:17:03 +0700

    merubah page error

---
+++ src/main/java/programmerzamannow/webMVC/controller/ErrorPageController.java
@@ -0,0 +1,30 @@
+package programmerzamannow.webMVC.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorPageController implements ErrorController{
+    
+    @RequestMapping(path = "/error")
+    public ResponseEntity<String> error(HttpServletRequest request){
+        Integer status = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String message = (String) request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+
+        String html = """
+            <html>
+            <body>
+            <h1> $status - $message </h1>
+            </body>
+            </html        
+                """.replace("$status", status.toString())
+                .replace("$message", message.toString());
+
+        return ResponseEntity.status(status).body(html);
+    }
+}
